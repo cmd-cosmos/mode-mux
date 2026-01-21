20601349270d74474d954abc6bbb69f067ffcec2
Premanshu Bhagat <premtsapj@gmail.com>
2026-01-20 19:45:15 -0500

    added required args for parser

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
 #pylint: skip-file
+
+import time
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-f", "--fun",   action="store_true", help="run fun mode")
+parser.add_argument("-s", "--study", action="store_true", help="run study mode")
+parser.add_argument("-w", "--work",  action="store_true", help="run work mode")
+
+args = parser.parse_args()
