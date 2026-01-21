df527010ba12d54b3f3a752305d23bb4cae783a7
Premanshu Bhagat <premtsapj@gmail.com>
2026-01-20 19:47:12 -0500

    basic structure and function declarations

--- src/modes.py
+++ src/modes.py
@@ -1,1 +1,22 @@
 #pylint: skip-file
+
+modes_map = {
+    0 : "fun", 
+    1 : "study", 
+    2 : "work" 
+}
+
+def fun():
+    pass
+
+def study():
+    pass
+
+def work():
+    pass
+
+name_func_map = {
+    "fun"  : fun,
+    "study": study,
+    "work" : work
+}
