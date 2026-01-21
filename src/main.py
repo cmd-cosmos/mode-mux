ca20a963b32f710ac838ad6ffb740993550ec134
Premanshu Bhagat <premtsapj@gmail.com>
2026-01-20 19:53:37 -0500

    added basic parser parsing and option to enter mode manually in case of missing args

--- src/main.py
+++ src/main.py
@@ -1,12 +1,34 @@
 #pylint: skip-file
+#type: ignore
 
-import time
 import argparse
+from modes import modes_map, name_func_map
 
-parser = argparse.ArgumentParser()
+def main():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("-f", "--fun",   action="store_true", help="run fun mode")
+    parser.add_argument("-s", "--study", action="store_true", help="run study mode")
+    parser.add_argument("-w", "--work",  action="store_true", help="run work mode")
+    
+    args = parser.parse_args()
+    
+    for name, func in name_func_map.items():
+        if getattr(args, name):
+            func()
+            return
+    
+    print("Choose Mode: ")
+    for num, name in modes_map.items():
+        print(f"{num} : {name.capitalize()}")
+    
+    choice = input("Enter Mode ID: ").strip()
+    try:
+        choice = int(choice)
+        mode = modes_map[choice]
+        name_func_map[mode]()
+    except (ValueError, KeyError):
+        print("Invalid Choice")
 
-parser.add_argument("-f", "--fun",   action="store_true", help="run fun mode")
-parser.add_argument("-s", "--study", action="store_true", help="run study mode")
-parser.add_argument("-w", "--work",  action="store_true", help="run work mode")
-
-args = parser.parse_args()
+if __name__ == "__main__":
+    main()
